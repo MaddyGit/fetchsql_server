3dab34a725cbd72e7ee1285603200389fc98fea8
Danish Dildar Ali <dani47mail@gmail.com>
2019-04-18 11:59:13 +0500

    modify index.js

--- index.js
+++ index.js
@@ -6,17 +6,21 @@ const host = 'localhost'
 const port = 5353
 
 app.use(Express.json())
+app.use( (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+})
 
 app.post('/getrs', (req, res) => {
     const query = req.body.query
-    
     getRS(query).then(rs => {
         res.send(JSON.stringify(rs))
         console.log('\nquery process success!\n')
     })
     .catch(err => {
         res.send(JSON.stringify(err))
-        console.log(`\nquery process failed!\n${err.name+': '+err.original.sqlMessage}\n\n`)
+        console.log(`\nquery process failed!\n${err}\n\n`)
     })
 })
 
